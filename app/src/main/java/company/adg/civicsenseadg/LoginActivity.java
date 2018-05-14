0e224ad29bbde8043ec9cdf3df115ae4a803c2aa
MasterChief94 <giacomocuccovillo94@gmail.com>
2018-05-14 10:00:41 +0200

    Initial commit

---
+++ app/src/main/java/company/adg/civicsenseadg/LoginActivity.java
@@ -0,0 +1,30 @@
+package company.adg.civicsenseadg;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button LoginButton = (Button) findViewById(R.id.LoginButton);
+        LoginButton.setOnClickListener(LoginButtonListener);
+    }
+
+    public View.OnClickListener LoginButtonListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+
+            Intent MainIntent = new Intent(LoginActivity.this, HomeActivity.class);
+            startActivity(MainIntent);
+        }
+
+    };
+}
